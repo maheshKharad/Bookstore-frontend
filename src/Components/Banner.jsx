8d9feb80655f95f4d6b6607ce03c491ec5c61489
mahesh kharad <mkharad20@gmail.com>
2024-09-26 14:37:39 +0530

    first commit

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -33,7 +33,7 @@ function Banner() {
         <div className="order-1 w-full mt-20 md:w-1/2">
           <img
             src={banner}
-            className="md:w-[550px] md:h-[460px] md:ml-12"
+            className="md:w-[550px]  md:h-[460px] md:ml-12"
             alt=""
           />
         </div>
